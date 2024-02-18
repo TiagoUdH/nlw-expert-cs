ff4956a815fd0f51a34cd38ab1730f624ae7f98e
Tiago Fernandes <tiagoferprojects@gmail.com>
2024-02-17 22:21:35 -0300

    Class 1

---
+++ src/RocketseatAuction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.API.Entitites;
+using RocketseatAuction.API.Repositories;
+
+namespace RocketseatAuction.API.UseCases.Auctions.GetCurrent;
+
+public class GetCurrentAuctionUseCase
+{
+    public Auction? Execute()
+    {
+        var repository = new RocketseatAuctionDbContext();
+
+        return repository.Auctions
+            .Include(auction => auction.Items)
+            .FirstOrDefault(auction => DateTime.UtcNow >= auction.Starts && DateTime.UtcNow <= auction.Ends);
+    }
+}
